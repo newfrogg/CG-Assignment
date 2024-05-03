9897dbabcb62cc48f5be57fd2215e0dd2cbb0912
newfrogg <hinh.nguyen2020@hcmut.edu.vn>
2024-05-03 23:16:47 +0700

    add script to test assignment-2011213.cpp file

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-g++ -g main.cpp -std=c++11 src/*.cpp -lGL -lGLU -lglut -lm -Iinclude -Isrc -o main
+# build project
+# g++ -g main.cpp -std=c++11 src/*.cpp -lGL -lGLU -lglut -lm -Iinclude -Isrc -o main
+# build assignment-2011213.cpp
+g++ -g assignment-2011213.cpp -std=c++11 -lGL -lGLU -lglut -lm -o assignment
